7518b46ad5e446025ba90030a1f6501794314eea
Luke Hammer <textchimp@gmail.com>
2019-04-02 21:17:46 +1100

    examples of key lookup

--- day05/lists-vs-dicts.py
+++ day05/lists-vs-dicts.py
@@ -20,3 +20,11 @@ student_dictionary = {
     },
     'scores': [70, 80, 60, 90, 100]
 }
+
+print('name:', student_dictionary['name'])
+print('age:', student_dictionary['age'])
+# nested dictionary access:
+print('street address:', student_dictionary['address']['street'])
+print('postcode:', student_dictionary['address']['postcode'])
+# list as value of key, access first element:
+print('first score:', student_dictionary['scores'][0])
